6c7ea0632e14f12de0aebc9a81bf88d3c79bc5d4
IsacVera <isaacvera37@icloud.com>
2024-05-27 21:00:10 -0700

    feat: concept for dynamic menu

--- index.js
+++ index.js
@@ -1,24 +1,97 @@
-calculatorSetting = "pace"
-measurement = document.querySelector('#measurement')
+let paceButton = document.querySelector('#paceButton')
+let distanceButton = document.querySelector('#distanceButton')
+let timeButton = document.querySelector('#timeButton')
 
-distanceInput = document.querySelector('#distanceInput')
-paceInput = document.querySelector('#paceInput')
-timeInput = document.querySelector('#timeInput')
+let menu = document.querySelector('#menu');
 
-paceButton = document.querySelector('#paceButton')
-distanceButton = document.querySelector('#distanceButton')
-timeButton = document.querySelector('#timeButton')
+//creating distance section of menu
+let distanceSection = document.createElement('div') 
+let distanceTopSection = document.createElement('div')
+let distanceHeader = document.createElement('h2')
+distanceHeader.innerHTML = 'Distance';
+let distanceSelect = document.createElement('select')
+distanceSelect.id = 'measurement';
+let optionMiles = document.createElement('option')
+optionMiles.value = 'miles';
+optionMiles.innerHTML = 'Miles';
+let optionKilometers = document.createElement('option')
+optionKilometers.value = 'kilometers';
+optionKilometers.innerHTML = 'Kilometers';
+distanceSelect.appendChild(optionMiles);
+distanceSelect.appendChild(optionKilometers);
+let distanceInputElement = document.createElement('input')
+distanceInputElement.id = 'distanceInput';
+distanceInputElement.type = 'text';
+
+distanceTopSection.appendChild(distanceHeader);
+distanceTopSection.appendChild(distanceSelect);
+
+distanceSection.appendChild(distanceTopSection);
+distanceSection.appendChild(distanceInputElement);
+
+
+//creating pace section of menu
+let paceSection = document.createElement('div')
+let paceHeader = document.createElement('h2')
+paceHeader.innerHTML = 'Pace';
+let paceInputElement = document.createElement('input')
+paceInputElement.id = 'paceInput';
+paceInputElement.type = 'text';
+paceInputElement.value = '00:00:0';
+
+paceSection.appendChild(paceHeader);
+paceSection.appendChild(paceInputElement);
+
+
+//creating time section of menu
+let timeSection = document.createElement('div')
+let timeHeader = document.createElement('h2')
+timeHeader.innerHTML = 'Time';
+let timeInputElement = document.createElement('input')
+timeInputElement.id = 'timeInput';
+timeInputElement.type = 'text';
+timeInputElement.value = '00:00:0';
+
+timeSection.appendChild(timeHeader);
+timeSection.appendChild(timeInputElement);
+
+
+//Default Menu
+menu.appendChild(timeSection);
+menu.appendChild(distanceSection);
+
+
+let calculatorSetting = "pace"
+let measurement = document.querySelector('#measurement')
 
 paceButton.addEventListener('click', function() {
     calculatorSetting = "pace";
+
+    while (menu.lastChild) {
+        menu.removeChild(menu.lastChild)
+    }
+    menu.appendChild(timeSection);
+    menu.appendChild(distanceSection);
 })
 
 distanceButton.addEventListener('click', function() {
     calculatorSetting = "distance";
+
+    while (menu.lastChild) {
+        menu.removeChild(menu.lastChild)
+    }
+    menu.appendChild(timeSection);
+    menu.appendChild(paceSection);
 })
 
 timeButton.addEventListener('click', function() {
     calculatorSetting = "time";
+
+    while (menu.lastChild) {
+        menu.removeChild(menu.lastChild)
+    }
+    menu.appendChild(distanceSection);
+    menu.appendChild(paceSection);
 })
 
 submitButton = document.querySelector('#submitInput')
@@ -102,8 +175,6 @@ function calculateTime(pace, distance) {
 
 
 function calculateDistance(time, pace) {
-    console.log(time)
-    console.log(pace)
     distance = time/pace;
 
     distance = Math.floor(distance*100000)/100000
@@ -113,6 +184,9 @@ function calculateDistance(time, pace) {
 
 submitButton.addEventListener('click', function(event) {
     if (calculatorSetting === "pace") {
+        let distanceInput = document.querySelector('#distanceInput')
+        let timeInput = document.querySelector('#timeInput')
+        
         time = timeToSeconds(timeInput.value);
         distance = Number(distanceInput.value);
 
@@ -121,14 +195,20 @@ submitButton.addEventListener('click', function(event) {
         console.log(paceString)
     }
     else if (calculatorSetting === "distance") {
+        let paceInput = document.querySelector('#paceInput')
+        let timeInput = document.querySelector('#timeInput')
+
         time = timeToSeconds(timeInput.value);
         pace = timeToSeconds(paceInput.value);
 
         distance = calculateDistance(time, pace);
-
+        console.log(distance)
     }
 
     else if (calculatorSetting === "time") {
+        let distanceInput = document.querySelector('#distanceInput')
+        let paceInput = document.querySelector('#paceInput')
+
         pace = timeToSeconds(paceInput.value);
         distance = Number(distanceInput.value);
 
